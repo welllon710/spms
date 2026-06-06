449c078da8e0301a1e2fe922b042772eaf962333
wuwei <18697703903@163.com>
2026-06-06 22:46:27 +0800

    chore: initial commit

---
+++ src/main/java/com/spms/SpmsApplication.java
@@ -0,0 +1,13 @@
+package com.spms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spms/SpmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
